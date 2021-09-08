c3833321b25556d3f323ac4639ae91e43cb502a6
Rama Aditya <46771792+elcodee@users.noreply.github.com>
2021-09-08 09:59:40 +0700

    logic tes

---
+++ index.js
@@ -0,0 +1,10 @@
+const n = 50;
+const word = ["Frontend", "Backend", "Forntend Backend"];
+
+
+const =
+
+for( i = 0;i < n; i ++) {
+    console.log("i", i);
+word.map(item => console.log("item", item))
+}
